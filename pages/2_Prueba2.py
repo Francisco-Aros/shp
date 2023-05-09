ac09f41d236b9d1d02a17e98869e15c4e95bcf22
Francisco-Aros <franciscoarosmunoz@gmail.com>
2023-05-08 22:34:20 -0400

    Prueba de visualización shp en web

---
+++ pages/2_Prueba2.py
@@ -0,0 +1,38 @@
+import geopandas as gpd
+import folium
+from streamlit_folium import folium_static
+from folium.plugins import GroupedLayerControl
+from folium.plugins import MarkerCluster
+
+# Cargar el archivo SHP en un objeto GeoDataFrame
+shp_path = "Cuenca_Valdivia.shp"
+gdf = gpd.read_file(shp_path)
+
+# Cargar el segundo archivo SHP en otro objeto GeoDataFrame
+shp2_path = "USO_suelo_LosLagos2006.shp"
+gdf2 = gpd.read_file(shp2_path)
+
+# Crear un objeto Folium Map centrado en los datos de los polígonos
+m = folium.Map(location=[-39.81, -73.2467], zoom_start=10)
+
+# Agregar los datos de los polígonos al mapa
+folium.GeoJson(gdf,
+               name='Polígonos',
+               tooltip=folium.GeoJsonTooltip(fields=['NOM_SSUBC', 'COD_SSUBC'], aliases=['Nombre subsub cuenca','Código subsub cuenca']),
+               overlay=True
+              ).add_to(m)
+
+# Agregar los datos de los polígonos del segundo archivo SHP al mapa
+folium.GeoJson(gdf2,
+               name='Capa_2',
+               tooltip=folium.GeoJsonTooltip(fields=['COMUNA','USO_ACTUAL'], aliases=['Comuna', 'Uso de suelo al 2006']),
+               overlay=True
+              ).add_to(m)
+#HASTA ESTE PUNTO SE PUEDEN VISUALIZAR BIEN LAS CAPAS, PERO HACE FALTA AGREGAR EL SELECCTOR DE CAPAS A LA VISUALIZACIÓN EN MAPA.
+
+
+# Mostrar el mapa en Streamlit
+folium_static(m)
+
+
+
