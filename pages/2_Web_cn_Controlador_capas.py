e257909466c87100835e6a18abf73864a50aaa49
Francisco-Aros <franciscoarosmunoz@gmail.com>
2023-05-11 17:46:40 -0400

    Cambio de nombre, pág con controlador utilizable

--- pages/2_Web_cn_Controlador_capas.py
+++ pages/2_Web_cn_Controlador_capas.py
@@ -1,9 +1,8 @@
 import geopandas as gpd
 import folium
 from streamlit_folium import folium_static
-from folium.plugins import GroupedLayerControl
-from folium.plugins import MarkerCluster
 import streamlit as st
+
 
 
 @st.cache_data() 
@@ -22,39 +21,51 @@ def get_data():
     gdf4 = gpd.read_file(shp4_path)
     return gdf, gdf2, gdf3, gdf4
 
+
+
 # Crear un objeto Folium Map centrado en los datos de los polígonos
 m = folium.Map(location=[-39.8, -72.8], zoom_start=9)
+
 
 # Agregar los datos de los polígonos al mapa
 # Obtener los datos de los polígonos y agregarlos al mapa
 gdf, gdf2, gdf3, gdf4 = get_data()
+
 folium.GeoJson(gdf,
-               name='Polígonos',
+               name='Uso de suelo en Valdivia al año 2006',
                tooltip=folium.GeoJsonTooltip(fields=['COMUNA','USO_ACTUAL'], aliases=['Comuna', 'Uso de suelo al 2006']),
-               overlay=True
+               show=False# no se moestrara la capa al cargar la página, de lo contrario si es que se desea que se pueda visualizar desde un inicio debe ser con el comando "overlay=True"
                ).add_to(m)
 
 # Agregar los datos de los polígonos del segundo archivo SHP al mapa
 folium.GeoJson(gdf2,
-               name='Capa_2',
+               name='Uso de suelo en Los Lagos al año 2006',
                tooltip=folium.GeoJsonTooltip(fields=['COMUNA','USO_ACTUAL'], aliases=['Comuna', 'Uso de suelo al 2006']),
-               overlay=True
+               show=False# no se moestrara la capa al cargar la página, de lo contrario si es que se desea que se pueda visualizar desde un inicio debe ser con el comando "overlay=True"
                ).add_to(m)
 
 #Agrega los datos de la tercera capa SHP al mapa
 folium.GeoJson(gdf3,
-               name='Capa_3',
+               name='Uso de suelo en Máfil al año 2006',
                tooltip=folium.GeoJsonTooltip(fields=['COMUNA','USO_ACTUAL'], aliases=['Comuna','Uso de suelo al 2006']),
-               overlay=True
+               show=False# no se moestrara la capa al cargar la página, de lo contrario si es que se desea que se pueda visualizar desde un inicio debe ser con el comando "overlay=True"
                ).add_to(m)
 
 #Agrega los datos de la cuarta capa SHP al mapa
 folium.GeoJson(gdf4,
-               name='Capa_4',
+               name='Uso de suelo en Panguipulli al año 2006',
                tooltip=folium.GeoJsonTooltip(fields=['COMUNA','USO_ACTUAL'], aliases=['Comuna','Uso de suelo al 2006']),
-               overlay=True
+               show=False# no se moestrara la capa al cargar la página, de lo contrario si es que se desea que se pueda visualizar desde un inicio debe ser con el comando "overlay=True"
                ).add_to(m)
 #HASTA ESTE PUNTO SE PUEDEN VISUALIZAR BIEN LAS CAPAS, PERO HACE FALTA AGREGAR EL SELECCTOR DE CAPAS A LA VISUALIZACIÓN EN MAPA.
+
+
+
+# Crear el control de capas agrupadas
+
+control = folium.LayerControl()
+
+control.add_to(m)
 
 
 # Mostrar el mapa en Streamlit
